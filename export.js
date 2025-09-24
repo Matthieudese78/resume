181718b451923829e5cc514f6c5332d3269a87f2
matthieu <66738778+Matthieudese78@users.noreply.github.com>
2025-09-24 18:40:47 +0200

    satisfactory

--- export.js
+++ export.js
@@ -6,11 +6,14 @@ const INPUT_HTML = process.argv[2] || "CV_Serre.html";
 const OUTPUT_PDF = process.argv[3] || INPUT_HTML.replace(/\.html?$/i, ".pdf");
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless:"new", args: ['--font-render-hinting=medium']});
   const page = await browser.newPage();
+  await page.emulateMediaType("screen");
 
   const filePath = `file:${path.join(__dirname, INPUT_HTML)}`;
   await page.goto(filePath, { waitUntil: "networkidle0" });
+
+  await page.setViewport({width: 794, height: 1123})
 
   // Set viewport to A4 width (approx. 794px wide at 96dpi) and automatic height
   // await page.setViewport({ width: 794, height: 1123 });
@@ -19,11 +22,13 @@ const OUTPUT_PDF = process.argv[3] || INPUT_HTML.replace(/\.html?$/i, ".pdf");
     path: OUTPUT_PDF,
     format: "A4",
     printBackground: true,
-    scale: 0.85, // Scale to fit A4 better
+    // displayHeaderFooter: false,
+    // scale: 0.99, // Scale to fit A4 better
     margin: { top: '20px', bottom: '20px', left: '20px', right: '20px' },
+    preferCSSPageSize: true,
   });
 
   await browser.close();
   console.log(`PDF saved as ${OUTPUT_PDF} (from ${INPUT_HTML})`);
-})();
+})().catch(console.error);
 
